4fd5100f5670f521447b075275502e1ebeac8a40
wasilisk <walbynow@gmail.com>
2023-02-05 22:57:49 +0300

    Completed Task 6

--- src/Answer/Main.java
+++ src/Answer/Main.java
@@ -1,4 +1,25 @@
 package Answer;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        // Задача была выполнена за 55 минут
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите число для построения таблицы умножения: ");
+        int num =  sc.nextInt();;
+        int[][] mas = new int[num+1][num+1];
+
+        for (int i = 1; i < mas.length; i++) {
+
+            System.out.print("\n");
+            for (int j = 1; j < mas[i].length; j++) {
+                int b = j * i;
+                mas[i][j]=b ;
+                System.out.printf("%2d ", mas[i][j]);
+            }
+
+        }
+    }
 }
